20db7edb125ddb7e1917e98cc5b246347b94c864
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-10-30 17:40:33 +0000

    Prepare for build

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="tna-utilities",
     version="0.0.1",
     author="Andrew Hosgood",
@@ -22,9 +22,8 @@ setuptools.setup(
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
     ],
-    packages=["tna_utilities"],
-    # package_data={
-    #     "": ["**/*.py", "**/*.html"],
-    # },
+    packages=find_packages(
+        include=["tna_utilities", "tna_utilities.*"], exclude=["tests", "tests.*"]
+    ),
     python_requires=">=3.9",
 )
