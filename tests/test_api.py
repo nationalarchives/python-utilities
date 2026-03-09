8276640acb0c1c0b029d95ec815ff28b134a6c6a
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-03-09 09:42:02 +0000

    Add basic API test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,10 @@
 import unittest
 from unittest import mock
-import requests
 
 from tna_utilities.api import ResourceForbidden, ResourceNotFound, SimpleJsonApiClient
 
 
-
-def mocked_get_requests(*args, **kwargs):
+def mocked_requests(*args, **kwargs):
     class MockResponse:
         def __init__(self, json_data, status_code):
             self.json_data = json_data
@@ -15,22 +13,22 @@ def mocked_get_requests(*args, **kwargs):
         def json(self):
             return self.json_data
 
-    if args[0] == 'http://mockapi.com/test':
-        return MockResponse({"key1": "value1"}, 200)
+    if args[0].upper() == "GET":
+        if args[1] == "http://mockapi.com/happy":
+            return MockResponse({"foo": "bar"}, 200)
 
     return MockResponse(None, 404)
 
 
-
 class TestSimpleJsonApiClient(unittest.TestCase):
-    @mock.patch('requests.get', side_effect=mocked_get_requests)
+    @mock.patch("requests.request", side_effect=mocked_requests)
     def test_happy(self, mock_get):
         client = SimpleJsonApiClient("http://mockapi.com/")
-        response = client.get("/test")
-        assert response == {"key1": "value1"}
+        response = client.get("/happy")
         self.assertEqual(type(response), dict)
-        self.assertDictEqual(response, {"key1": "value1"})
-        self.assertIn(mock.call('http://mockapi.com/test'), mock_get.call_args_list)
+        self.assertDictEqual(response, {"foo": "bar"})
+        self.assertIn(
+            mock.call("GET", "http://mockapi.com/happy", params={}, headers={}),
+            mock_get.call_args_list,
+        )
         self.assertEqual(len(mock_get.call_args_list), 1)
-
-
