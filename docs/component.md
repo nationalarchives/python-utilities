eec79f04ea88acf83a217ed13a6a86d4e0ad9539
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-05-12 15:47:18 +0100

    Add pagination functionality and support for pagination component in TNA Frontend (#32)

---
+++ docs/component.md
@@ -0,0 +1,57 @@
+# Component
+
+## `paginate()`
+
+Creates a pagination object in accordance with the [pages to show in a pagination component](https://design-system.nationalarchives.gov.uk/components/pagination/#number-of-page-links) in the National Archives Design System.
+
+### Arguments
+
+| Argument       | Description                                                | Default |
+| -------------- | ---------------------------------------------------------- | ------- |
+| `pages`        | The total number of pages to paginate                      | [none]  |
+| `current_page` | The number of the current page                             | [none]  |
+| `around`       | The number of items to always show around the current page | `1`     |
+
+### Example
+
+```python
+from tna_utilities.component import paginate
+
+print(paginate(42, 7))
+# [1, "...", 6, 7, 8, "...", 42]
+
+print(paginate(42, 7, around=2))
+# [1, "...", 5, 6, 7, 8, 9, "...", 42]
+```
+
+## `tna_frontend_pagination_items()`
+
+Creates an object that be used directly in a [National Archives pagination component](https://design-system.nationalarchives.gov.uk/components/pagination/).
+
+### Arguments
+
+| Argument       | Description                                                                     | Default                                                            |
+| -------------- | ------------------------------------------------------------------------------- | ------------------------------------------------------------------ |
+| `pages`        | The total number of pages to paginate                                           | [none]                                                             |
+| `current_page` | The number of the current page                                                  | [none]                                                             |
+| `base_url`     | The base URL including the blank query string for the page                      | [none]                                                             |
+| `around`       | The number of items to always show around the current page                      | `1`                                                                |
+| `transformer`  | A function to create the item given a number and whether it is the current page | `tna_utilities.component.tna_frontend_pagination_item_transformer` |
+| `ellipsis`     | A dictionary to use in place of an ellipsis                                     | `{"ellipsis": True}`                                               |
+
+### Example
+
+```python
+from tna_utilities.component import paginate
+
+print(tna_frontend_pagination_items(42, 6, "?page="))
+# [
+#     {"number": 1, "current": False, "href": "?page=1"},
+#     {"ellipsis": True},
+#     {"number": 5, "current": False, "href": "?page=5"},
+#     {"number": 6, "current": True, "href": "?page=6"},
+#     {"number": 7, "current": False, "href": "?page=7"},
+#     {"ellipsis": True},
+#     {"number": 42, "current": False, "href": "?page=42"},
+# ]
+```
---
+++ tests/test_component.py
@@ -0,0 +1,401 @@
+import unittest
+
+from tna_utilities.component import (
+    PAGINATION_GAP,
+    paginate,
+    tna_frontend_pagination_items,
+)
+
+
+class TestComponent(unittest.TestCase):
+    def test_pagination_first(self):
+        self.assertEqual(
+            paginate(42, 1),
+            [
+                1,
+                2,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_second(self):
+        self.assertEqual(
+            paginate(42, 2),
+            [
+                1,
+                2,
+                3,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_third(self):
+        self.assertEqual(
+            paginate(42, 3),
+            [
+                1,
+                2,
+                3,
+                4,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_fourth(self):
+        self.assertEqual(
+            paginate(42, 4),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_fifth(self):
+        self.assertEqual(
+            paginate(42, 5),
+            [
+                1,
+                PAGINATION_GAP,
+                4,
+                5,
+                6,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_sixth(self):
+        self.assertEqual(
+            paginate(42, 6),
+            [
+                1,
+                PAGINATION_GAP,
+                5,
+                6,
+                7,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_fifth_from_last(self):
+        self.assertEqual(
+            paginate(42, 37),
+            [
+                1,
+                PAGINATION_GAP,
+                36,
+                37,
+                38,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_four_from_last(self):
+        self.assertEqual(
+            paginate(42, 38),
+            [
+                1,
+                PAGINATION_GAP,
+                37,
+                38,
+                39,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_three_from_last(self):
+        self.assertEqual(
+            paginate(42, 39),
+            [
+                1,
+                PAGINATION_GAP,
+                38,
+                39,
+                40,
+                41,
+                42,
+            ],
+        )
+
+    def test_pagination_two_from_last(self):
+        self.assertEqual(
+            paginate(42, 40),
+            [
+                1,
+                PAGINATION_GAP,
+                39,
+                40,
+                41,
+                42,
+            ],
+        )
+
+    def test_pagination_one_from_last(self):
+        self.assertEqual(
+            paginate(42, 41),
+            [
+                1,
+                PAGINATION_GAP,
+                40,
+                41,
+                42,
+            ],
+        )
+
+    def test_pagination_last(self):
+        self.assertEqual(
+            paginate(42, 42),
+            [
+                1,
+                PAGINATION_GAP,
+                41,
+                42,
+            ],
+        )
+
+    def test_pagination_first_larger_around(self):
+        self.assertEqual(
+            paginate(42, 1, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_second_larger_around(self):
+        self.assertEqual(
+            paginate(42, 2, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_third_larger_around(self):
+        self.assertEqual(
+            paginate(42, 3, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_fourth_larger_around(self):
+        self.assertEqual(
+            paginate(42, 4, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_fifth_larger_around(self):
+        self.assertEqual(
+            paginate(42, 5, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_sixth_larger_around(self):
+        self.assertEqual(
+            paginate(42, 6, around=3),
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_seventh_larger_around(self):
+        self.assertEqual(
+            paginate(42, 7, around=3),
+            [
+                1,
+                PAGINATION_GAP,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_first_no_around(self):
+        self.assertEqual(
+            paginate(42, 1, around=0),
+            [
+                1,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_second_no_around(self):
+        self.assertEqual(
+            paginate(42, 2, around=0),
+            [
+                1,
+                2,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_third_no_around(self):
+        self.assertEqual(
+            paginate(42, 3, around=0),
+            [
+                1,
+                PAGINATION_GAP,
+                3,
+                PAGINATION_GAP,
+                42,
+            ],
+        )
+
+    def test_pagination_no_pages(self):
+        with self.assertRaises(Exception):
+            paginate(0, 1)
+
+    def test_pagination_invalid_pages(self):
+        with self.assertRaises(Exception):
+            paginate(None, 1)
+
+    def test_pagination_negative_current_page(self):
+        with self.assertRaises(Exception):
+            paginate(42, -1)
+
+    def test_pagination_negative_around(self):
+        with self.assertRaises(Exception):
+            paginate(42, 1, around=-1)
+
+    def test_tna_pagination_items(self):
+        self.assertEqual(
+            tna_frontend_pagination_items(42, 6, "/test?page="),
+            [
+                {"number": 1, "current": False, "href": "/test?page=1"},
+                {"ellipsis": True},
+                {"number": 5, "current": False, "href": "/test?page=5"},
+                {"number": 6, "current": True, "href": "/test?page=6"},
+                {"number": 7, "current": False, "href": "/test?page=7"},
+                {"ellipsis": True},
+                {"number": 42, "current": False, "href": "/test?page=42"},
+            ],
+        )
+
+    def test_tna_pagination_items_custom_transformer(self):
+        def custom_transformer(item, current_page, base_url):
+            return {
+                "page": item,
+                "is_current": item == current_page,
+                "url": f"{base_url}{item}",
+            }
+
+        self.assertEqual(
+            tna_frontend_pagination_items(
+                42, 6, "/test?page=", transformer=custom_transformer
+            ),
+            [
+                {"page": 1, "is_current": False, "url": "/test?page=1"},
+                {"ellipsis": True},
+                {"page": 5, "is_current": False, "url": "/test?page=5"},
+                {"page": 6, "is_current": True, "url": "/test?page=6"},
+                {"page": 7, "is_current": False, "url": "/test?page=7"},
+                {"ellipsis": True},
+                {"page": 42, "is_current": False, "url": "/test?page=42"},
+            ],
+        )
+
+    def test_tna_pagination_items_custom_transformer_lambda(self):
+        self.assertEqual(
+            tna_frontend_pagination_items(
+                42,
+                6,
+                "/test?page=",
+                transformer=lambda item, current_page, base_url: {
+                    "page": item,
+                    "is_current": item == current_page,
+                    "url": f"{base_url}{item}",
+                },
+            ),
+            [
+                {"page": 1, "is_current": False, "url": "/test?page=1"},
+                {"ellipsis": True},
+                {"page": 5, "is_current": False, "url": "/test?page=5"},
+                {"page": 6, "is_current": True, "url": "/test?page=6"},
+                {"page": 7, "is_current": False, "url": "/test?page=7"},
+                {"ellipsis": True},
+                {"page": 42, "is_current": False, "url": "/test?page=42"},
+            ],
+        )
+
+    def test_tna_pagination_items_custom_ellipsis(self):
+        self.assertEqual(
+            tna_frontend_pagination_items(
+                42, 6, "/test?page=", ellipsis={"number": None}
+            ),
+            [
+                {"number": 1, "current": False, "href": "/test?page=1"},
+                {"number": None},
+                {"number": 5, "current": False, "href": "/test?page=5"},
+                {"number": 6, "current": True, "href": "/test?page=6"},
+                {"number": 7, "current": False, "href": "/test?page=7"},
+                {"number": None},
+                {"number": 42, "current": False, "href": "/test?page=42"},
+            ],
+        )
