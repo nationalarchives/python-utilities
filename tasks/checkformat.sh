c838fd005555b844d2171df2112388324af9e659
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-05-14 13:07:13 +0100

    Update ruff version in checkformat script to use test image

--- tasks/checkformat.sh
+++ tasks/checkformat.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -v "$(pwd)":/app/ ghcr.io/nationalarchives/tna-python-dev:preview checkformat
+# TODO: Change ruff version back to preview once the Docker image has been released
+docker run --rm -v "$(pwd)":/app/ ghcr.io/nationalarchives/tna-python-dev:ruff checkformat
